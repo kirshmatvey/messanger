ee5077c27348ab36a51be1a85271856fff96c871
kirshmatvey <matveykirshchin01@gmail.com>
2026-06-07 23:10:29 +0400

    mistakes corrected

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -10,7 +10,7 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
             return action.payload === 'up' ? state.sort((a, b) => a.name.localeCompare(b.name)) : state.sort((a, b) => -a.name.localeCompare(b.name)) // need to fix
         }
         case 'check': {
-            return state.filter((user) => user.age >= 18)  // need to fix
+            return state.filter((user) => user.age >= 18).sort((a, b) => a.age - b.age)  // need to fix
         }
         default:
             return state
