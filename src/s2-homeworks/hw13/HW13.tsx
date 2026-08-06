7b06b1b3a9909bbbd2cf000b5c941bc910b2368d
kirshmatvey <matveykirshchin01@gmail.com>
2026-08-06 18:04:39 +0400

    создан обÐработчик оÐшибок

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -34,7 +34,6 @@ const HW13 = () => {
         axios
             .post(url, {success: x})
             .then((res) => {
-                console.log(res)
                 setCode(`Код ${res.status}!`)
                 setImage(success200)
                 setText(res.data.errorText)
@@ -43,7 +42,6 @@ const HW13 = () => {
 
             })
             .catch((e) => {
-                console.log(e)
                 if (e.response.status === 500) {
                     setCode(`Код ${e.response.status}!`)
                     setImage(error500)
